c8cd06dd6fdfc3fa4f7515d0afe61b6fa767f2e5
Dmitriy Kolesnikov <xxsnakerxx@gmail.com>
2016-03-28 19:40:49 +0600

    Support RN 0.22

--- example/android/app/src/main/java/com/socialauthexample/MainActivity.java
+++ example/android/app/src/main/java/com/socialauthexample/MainActivity.java
@@ -1,7 +1,5 @@
 package com.socialauthexample;
 
-import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -12,6 +10,7 @@ import java.util.List;
 import com.xxsnakerxx.socialauth.SocialAuthPackage;
 
 public class MainActivity extends ReactActivity {
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
