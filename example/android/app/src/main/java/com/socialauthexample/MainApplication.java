07da7ee6e2e980dc9ffd99c6ad5f16f5df97174d
Dmitriy Kolesnikov <xxsnakerxx@gmail.com>
2017-08-02 21:34:34 +0600

    Updated example deps

--- example/android/app/src/main/java/com/socialauthexample/MainApplication.java
+++ example/android/app/src/main/java/com/socialauthexample/MainApplication.java
@@ -1,15 +1,15 @@
 package com.socialauthexample;
 
+import com.xxsnakerxx.socialauth.SocialAuthPackage;
+
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.xxsnakerxx.socialauth.SocialAuthPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.ReactInstanceManager;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,7 +18,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
