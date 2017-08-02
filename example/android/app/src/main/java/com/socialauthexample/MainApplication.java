d3bceb3563c48ec79b27f3556542f0a90c6a7038
Dmitriy Kolesnikov <xxsnakerxx@gmail.com>
2017-08-02 22:38:48 +0600

    Fixed running example on android

--- example/android/app/src/main/java/com/socialauthexample/MainApplication.java
+++ example/android/app/src/main/java/com/socialauthexample/MainApplication.java
@@ -9,7 +9,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.facebook.react.ReactInstanceManager;
 
 import java.util.Arrays;
 import java.util.List;
